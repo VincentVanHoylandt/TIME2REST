bbcf183b5450932fb2aba8aca2a59018b750c94f
Christian Bondzie <christian.zap@hotmail.fr>
2023-03-08 15:47:11 +0100

    Crétation du model et du controller Diploma, ajout des validation sur les champs obligatoire au moment de la création

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_134934) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_143922) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "diplomas", force: :cascade do |t|
+    t.date "start_time"
+    t.date "end_time"
+    t.string "title"
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_diplomas_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_134934) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "diplomas", "users"
 end
