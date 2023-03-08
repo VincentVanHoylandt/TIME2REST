81222aa283f8c5ad325c953ede88c802808977f0
vincentvanhoylandt <vincent.vanhoylandt@gmail.com>
2023-03-08 17:10:39 +0100

    Added review model controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_155224) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_160841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -58,6 +58,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_155224) do
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.bigint "user_id", null: false
+    t.bigint "appliance_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["appliance_id"], name: "index_reviews_on_appliance_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "seens", force: :cascade do |t|
     t.bigint "offer_id", null: false
     t.bigint "user_id", null: false
@@ -87,6 +98,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_155224) do
   add_foreign_key "appliances", "users"
   add_foreign_key "diplomas", "users"
   add_foreign_key "offers", "users"
+  add_foreign_key "reviews", "appliances"
+  add_foreign_key "reviews", "users"
   add_foreign_key "seens", "offers"
   add_foreign_key "seens", "users"
 end
