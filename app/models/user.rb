78fc2ec3cfb70fb02159f546b1542b4653ebe8ea
philippe goulliart <philippe.goulliart62@gmail.com>
2023-03-08 16:56:01 +0100

    added appliances

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :offers, dependent: :destroy
   has_many :seens, dependent: :destroy
+  has_many :appliances, dependents: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :job_title, :first_name, :last_name, :age, presence: true
