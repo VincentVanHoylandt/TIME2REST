910c25230c2c77be57d9d86f6cfda72b008cbe7c
jeremy twardowski <kherset@protonmail.com>
2023-03-10 10:38:04 +0100

    pundit

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,31 +1,47 @@
 class OffersController < ApplicationController
+  before_action :set_offer, only: [:update, :edit, :destroy, :show]
+
   def new
+    @offer = Offer.new
+    authorize @offer
   end
 
   def create
+    @offer = Offer.new(offer_params)
+    @offer.user = current_user
+    authorize @offer
   end
 
   def index
+    @offers = policy_scope(Offer)
     if params[:job_title].present? && params[:address].present?
       @offers = Offer.where("job_title ILIKE ?", "%#{params[:job_title]}%").where("address ILIKE ?", "%#{params[:address]}%")
     elsif params[:job_title].present?
       @offers = Offer.where("job_title ILIKE ?", "%#{params[:job_title]}%")
     elsif params[:address].present?
       @offers = Offer.where("address ILIKE ?", "%#{params[:address]}%")
-    else
-      @offers = Offer.all
     end
   end
 
   def show
+    authorize @offer
   end
 
   def edit
+    authorize @offer
   end
 
   def update
+    authorize @offer
   end
 
   def destroy
+    authorize @offer
+  end
+
+  private
+
+  def set_offer
+    @offer = Offer.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,6 @@ Rails.application.routes.draw do
   resources :participants, only: :create
   resources :seens, only: %i[index create]
 
-
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
