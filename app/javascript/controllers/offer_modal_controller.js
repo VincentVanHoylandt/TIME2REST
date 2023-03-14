c377233dba32b68c252662ec8e0707eb0f7760a0
philippe goulliart <philippe.goulliart62@gmail.com>
2023-03-14 16:51:44 +0100

    offer show done

--- app/javascript/controllers/offer_modal_controller.js
+++ app/javascript/controllers/offer_modal_controller.js
@@ -1,19 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
 
+
 // Connects to data-controller="offer-modal"
 export default class extends Controller {
-  static targets = ['index']
+  static targets = ['index', 'content']
   connect() {
   }
 
   show() {
-    event.preventDefault()
-    this.indexTarget.classList.remove('d-none')
+    event.preventDefault();
+    this.indexTarget.classList.remove('d-none');
   }
 
   dont_show() {
-    event.preventDefault()
-    this.indexTarget.classList.add('d-none')
+
+  event.preventDefault();
+  this.contentTarget.classList.add('right-shift');
+  console.log('avant la fonction');
+  this.contentTarget.addEventListener('animationend', () => {
+    this.indexTarget.classList.add('d-none');
+  }, { once: true });
+
+  this.contentTarget.addEventListener('animationend', () => {
+    this.contentTarget.classList.remove('right-shift');
+  }, { once: true });
   }
 
 }
