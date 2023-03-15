a1a755a70bc88a38ee4702821c1dd1e02d73f3fa
jeremy twardowski <kherset@protonmail.com>
2023-03-15 16:18:09 +0100

    seeds modifs, profil dynamique avec user, retrait de mise en forme de prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,59 +8,59 @@ puts "Creating users..."
 
 user1 = User.create({ first_name: "Vincent", last_name: "Vanhoylandt", age: 27, email: "vincent@lewagon.fr",
                       password: "lewagon", job_title: "Développeur Web" })
-diplomas1 = Diploma.create(user: user1, start_time: "1er septembre 2022", end_time: "30 juin 2024", title: "Diplôme d'Ingénieur en Développement Web", description: "Formation approfondie sur les technologies web, l'architecture logicielle et la sécurité.")
-experiences1 = Experience.create(user: user1, start_time: "1er juillet 2020", end_time: "30 juin 2022", title: "Développeur Front-end chez ABC Company", description: "Création et maintenance de sites web réactifs et optimisés pour les mobiles.")
+diplomas1 = Diploma.create(user: user1, start_time: "01-09-2022", end_time: "29/02/2023", title: "Diplôme d'Ingénieur en Développement Web", description: "Formation approfondie sur les technologies web, l'architecture logicielle et la sécurité.")
+experiences1 = Experience.create(user: user1, start_time: "01-07-2020", end_time: "30-06-2022", title: "Développeur Front-end chez ABC Company", description: "Création et maintenance de sites web réactifs et optimisés pour les mobiles.")
 
 
 user2 = User.create({ first_name: "Christian", last_name: "Bondzie", age: 44, email: "christian@lewagon.fr",
                       password: "lewagon", job_title: "Architecte d'intérieur" })
-diplomas2 = Diploma.create(user: user2, start_time: "1er septembre 2019", end_time: "30 juin 2020", title: "Diplôme en Architecture d'Intérieur", description: "Formation en design d'intérieur, dessin technique et gestion de projet.")
-experiences2 = Experience.create(user: user2, start_time: "1er juillet 2021", end_time: "30 janvier 2023", title: "Architecte d'Intérieur chez ABC Design", description: "Conception de plans d'aménagement et de design intérieur pour des projets résidentiels et commerciaux.")
+diplomas2 = Diploma.create(user: user2, start_time: "01-09-2019", end_time: "30-06-2020", title: "Diplôme en Architecture d'Intérieur", description: "Formation en design d'intérieur, dessin technique et gestion de projet.")
+experiences2 = Experience.create(user: user2, start_time: "01-07-2021", end_time: "30-01-2023", title: "Architecte d'Intérieur chez ABC Design", description: "Conception de plans d'aménagement et de design intérieur pour des projets résidentiels et commerciaux.")
 
 user3 = User.create({ first_name: "Philippe", last_name: "Goulliard", age: 49, email: "philippe@lewagon.fr",
                       password: "lewagon", job_title: "Infirmier à domicile" })
-diplomas3 = Diploma.create(user: user3, start_time: "1er septembre 2016", end_time: "30 juin 2019", title: "Diplôme d'État d'Infirmier", description: "Formation en soins infirmiers, pharmacologie et hygiène hospitalière.")
-experiences3 = Experience.create(user: user3, start_time: "1er juillet 2019", end_time: "30 juin 2021", title: "Infirmier à domicile chez ABC Santé", description: "Soins infirmiers à domicile pour des patients atteints de maladies chroniques.")
+diplomas3 = Diploma.create(user: user3, start_time: "01-09-2016", end_time: "30-06-2019", title: "Diplôme d'État d'Infirmier", description: "Formation en soins infirmiers, pharmacologie et hygiène hospitalière.")
+experiences3 = Experience.create(user: user3, start_time: "01-07-2019", end_time: "30-06-2021", title: "Infirmier à domicile chez ABC Santé", description: "Soins infirmiers à domicile pour des patients atteints de maladies chroniques.")
 
 user4 = User.create({ first_name: "Jeremy", last_name: "Twardowski", age: 36, email: "jeremy@lewagon.fr",
                       password: "lewagon", job_title: "Electricien" })
-diplomas4 = Diploma.create(user: user4, start_time: "1er septembre 2013", end_time: "30 juin 2016", title: "BEP Électrotechnique", description: "Formation en installation électrique, maintenance et dépannage des équipements électriques." )
-experiences4 = Experience.create(user: user4, start_time: "1er juillet 2016", end_time: "30 juin 2020", title: "Électricien chez DEF Électricité", description: "Installation, maintenance et réparation des équipements électriques pour les clients résidentiels et commerciaux.")
+diplomas4 = Diploma.create!(user: user4, start_time: "30-06-2013", end_time: "30-06-2016", title: "BEP Électrotechnique", description: "Formation en installation électrique, maintenance et dépannage des équipements électriques." )
+experiences4 = Experience.create(user: user4, start_time: "01-07-2016", end_time: "30-06-2020", title: "Électricien chez DEF Électricité", description: "Installation, maintenance et réparation des équipements électriques pour les clients résidentiels et commerciaux.")
 
 user5 = User.create({ first_name: "Sophie", last_name: "Koch", age: 52, email: "sophie@lewagon.fr",
                       password: "lewagon", job_title: "Psychologue clinicien" })
-diplomas5 = Diploma.create(user: user5, start_time: "1er septembre 2008", end_time: "30 juin 2013", title: "Master de Psychologie Clinique", description: "Formation en psychologie clinique, thérapies cognitives et comportementales et approches psychodynamiques.")
-experiences5 = Experience.create(user: user5, start_time: "1er juillet 2013", end_time: "30 juin 2019", title: "Psychologue clinicien chez Cabinet de Psychologie XYZ", description: "Évaluation et traitement de patients souffrant de troubles psychologiques tels que l'anxiété, la dépression et les troubles alimentaires." )
+diplomas5 = Diploma.create(user: user5, start_time: "01-09-2008", end_time: "30-06-2013", title: "Master de Psychologie Clinique", description: "Formation en psychologie clinique, thérapies cognitives et comportementales et approches psychodynamiques.")
+experiences5 = Experience.create(user: user5, start_time: "01-07-2013", end_time: "30-06-2019", title: "Psychologue clinicien chez Cabinet de Psychologie XYZ", description: "Évaluation et traitement de patients souffrant de troubles psychologiques tels que l'anxiété, la dépression et les troubles alimentaires." )
 
 user6 = User.create({ first_name: "Maximilian", last_name: "Bauer", age: 22, email: "maximilian@lewagon.fr",
                       password: "lewagon", job_title: "Chef de projet informatique" })
-diplomas6 = Diploma.create(user: user6, start_time: "1er septembre 2010", end_time: "30 juin 2014", title: "Master en Ingénierie Informatique", description: "Formation en gestion de projet informatique, développement de logiciels et bases de données." )
-experiences6 = Experience.create(user: user6, start_time: "1er juillet 2014", end_time: "30 juin 2020", title: "Chef de projet informatique chez ABC Informatique", description: "Gestion de projets informatiques pour les clients du secteur de la finance, développement de logiciels et suivi de l'avancement des projets." )
+diplomas6 = Diploma.create(user: user6, start_time: "01-09-2010", end_time: "30-06-2014", title: "Master en Ingénierie Informatique", description: "Formation en gestion de projet informatique, développement de logiciels et bases de données." )
+experiences6 = Experience.create(user: user6, start_time: "01-07-2014", end_time: "30-06-2020", title: "Chef de projet informatique chez ABC Informatique", description: "Gestion de projets informatiques pour les clients du secteur de la finance, développement de logiciels et suivi de l'avancement des projets." )
 
 user7 = User.create({ first_name: "Maria", last_name: "Schmidt", age: 39, email: "maria@lewagon.fr",
                       password: "lewagon", job_title: "Traducteur" })
-diplomas7 = Diploma.create(user: user7, start_time: "1er septembre 2006", end_time: "30 juin 2010", title: "Licence en Langues étrangères appliquées", description: "Formation en langues étrangères, traduction et interprétation.")
-experiences7 = Experience.create(user: user7, start_time: "1er juillet 2010", end_time: "30 juin 2015", title: "Traducteur chez Agence de traduction XYZ", description: "Traduction de documents juridiques et financiers de l'anglais vers le français pour les clients de l'agence." )
+diplomas7 = Diploma.create(user: user7, start_time: "01-09-2006", end_time: "30-06-2010", title: "Licence en Langues étrangères appliquées", description: "Formation en langues étrangères, traduction et interprétation.")
+experiences7 = Experience.create(user: user7, start_time: "01-07-2010", end_time: "30-06-2015", title: "Traducteur chez Agence de traduction XYZ", description: "Traduction de documents juridiques et financiers de l'anglais vers le français pour les clients de l'agence." )
 
 user8 = User.create({ first_name: "Julie", last_name: "Lefèvre", age: 32, email: "julie@lewagon.fr",
                        password: "lewagon", job_title: "Infirmiere à domicile" })
-diplomas8 = Diploma.create(user: user8, start_time: "1er septembre 2008", end_time: "30 juin 2011", title: "Diplôme d'Etat d'Infirmier", description: "Formation en soins infirmiers, pharmacologie, anatomie et physiologie.")
-experiences8 = Experience.create(user: user8, start_time: "1er juillet 2011", end_time: "30 juin 2015", title: "Infirmier à domicile chez Soins à Domicile SARL", description: "Soins à domicile pour les patients atteints de maladies chroniques, gestion de cas, et coordination avec les médecins.")
+diplomas8 = Diploma.create(user: user8, start_time: "01-09-2008", end_time: "30-06-2011", title: "Diplôme d'Etat d'Infirmier", description: "Formation en soins infirmiers, pharmacologie, anatomie et physiologie.")
+experiences8 = Experience.create(user: user8, start_time: "01-07-2011", end_time: "30-06-2015", title: "Infirmier à domicile chez Soins à Domicile SARL", description: "Soins à domicile pour les patients atteints de maladies chroniques, gestion de cas, et coordination avec les médecins.")
 
 user9 = User.create({ first_name: "Victor", last_name: "Bouchard", age: 48, email: "victor@lewagon.fr",
                        password: "lewagon", job_title: "Infirmier à domicile" })
-diplomas9 = Diploma.create(user: user9, start_time: "1er septembre 2012", end_time: "30 juin 2015", title: "Licence en Sciences Infirmières", description: "Formation en soins infirmiers, psychologie, et en santé publique.")
-experiences9 = Experience.create(user: user9, start_time: "1er juillet 2015", end_time: "30 juin 2017", title: "Infirmier de nuit chez Assistance à Domicile SAS", description: "Soins infirmiers de nuit pour les patients âgés, gestion de cas, et assistance aux activités quotidiennes.")
+diplomas9 = Diploma.create(user: user9, start_time: "01-09-2012", end_time: "30-06-2015", title: "Licence en Sciences Infirmières", description: "Formation en soins infirmiers, psychologie, et en santé publique.")
+experiences9 = Experience.create(user: user9, start_time: "01-07-2015", end_time: "30-06-2017", title: "Infirmier de nuit chez Assistance à Domicile SAS", description: "Soins infirmiers de nuit pour les patients âgés, gestion de cas, et assistance aux activités quotidiennes.")
 
 user10 = User.create({ first_name: "Berangere", last_name: "Picard", age: 22, email: "berangere@lewagon.fr",
                        password: "lewagon", job_title: "Infirmiere à domicile" })
-diplomas10 = Diploma.create(user: user10, start_time: "1er septembre 2016", end_time: "30 juin 2018", title: "Certificat en Soins Palliatifs", description: "Formation spécialisée en soins palliatifs pour les patients atteints de maladies graves.")
-experiences10 = Experience.create(user: user10, start_time: "1er juillet 2018", end_time: "30 juin 2020", title: "Infirmier à domicile indépendant", description: "Soins à domicile pour les patients atteints de maladies chroniques, gestion de cas, et coordination avec les médecins.")
+diplomas10 = Diploma.create(user: user10, start_time: "01-09-2016", end_time: "30-06-2018", title: "Certificat en Soins Palliatifs", description: "Formation spécialisée en soins palliatifs pour les patients atteints de maladies graves.")
+experiences10 = Experience.create(user: user10, start_time: "01-07-2018", end_time: "30-06-2020", title: "Infirmier à domicile indépendant", description: "Soins à domicile pour les patients atteints de maladies chroniques, gestion de cas, et coordination avec les médecins.")
 
 user11 = User.create({ first_name: "Maxime", last_name: "Bergeron", age: 36, email: "maxime@lewagon.fr",
                        password: "lewagon", job_title: "Infirmier à domicile" })
-diplomas11 = Diploma.create(user: user11, start_time: "1er septembre 2020", end_time: "30 juin 2022", title: "Master en Soins Infirmiers à Domicile", description: "Formation spécialisée en soins à domicile, gestion de cas, et en éducation des patients et de leurs familles.")
-experiences11 = Experience.create(user: user11, start_time: "1er juillet 2022", end_time: "30 juin 2023", title: "Infirmier de soins palliatifs chez Maison de Soins Palliatifs", description: "Soins infirmiers palliatifs pour les patients atteints de maladies graves en phase terminale.")
+diplomas11 = Diploma.create(user: user11, start_time: "01-09-2020", end_time: "30-06-2022", title: "Master en Soins Infirmiers à Domicile", description: "Formation spécialisée en soins à domicile, gestion de cas, et en éducation des patients et de leurs familles.")
+experiences11 = Experience.create(user: user11, start_time: "01-07-2022", end_time: "30-06-2023", title: "Infirmier de soins palliatifs chez Maison de Soins Palliatifs", description: "Soins infirmiers palliatifs pour les patients atteints de maladies graves en phase terminale.")
 
 puts "users created"
 
